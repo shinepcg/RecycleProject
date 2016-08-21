b0f4609ceb40a2757442581f934109f8b75dc8fd
shinepcg <shinepcg@naver.com>
2016-08-21 17:43:40 +0900

    스크립트 추가

---
+++ Assets/Script/MainUiCanvas.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainUiCanvas : MonoBehaviour {
+
+    Text messageText;
+    Text enableTestText;
+    void Awake()
+    {
+        messageText = transform.FindChild("MessageText").GetComponent<Text>();
+        enableTestText = transform.FindChild("EnableTestText").GetComponent<Text>();
+        enableTestText.enabled = false;
+    }
+
+
+    public void MessageStart()
+    {
+        print("MessageStart");
+        messageText.text = "hello";
+        enableTestText.enabled = true;
+
+    }
+
+
+
+
+}
